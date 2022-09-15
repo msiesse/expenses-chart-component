e791d5d958914e6f3ac5c33cc3e8e4e8c91d0586
msiesse <msiesse@corefortech.com>
2022-09-15 19:28:47 +0200

    add basic histogram sticks with different height according to json

---
+++ scripts/main.js
@@ -0,0 +1,38 @@
+const data = [
+    {
+        "day": "mon",
+        "amount": 17.45
+    },
+    {
+        "day": "tue",
+        "amount": 34.91
+    },
+    {
+        "day": "wed",
+        "amount": 52.36
+    },
+    {
+        "day": "thu",
+        "amount": 31.07
+    },
+    {
+        "day": "fri",
+        "amount": 23.39
+    },
+    {
+        "day": "sat",
+        "amount": 43.28
+    },
+    {
+        "day": "sun",
+        "amount": 25.48
+    }
+]
+
+const weekdays = ['mon', 'tue', 'wed', 'thu', 'fri', 'sat', 'sun']
+const maxAmount = Math.max(...data.map((elem) => elem.amount))
+weekdays.forEach((day, index) => {
+    const elem = document.getElementsByClassName(day)[0]
+    elem.style.height = `${(data[index].amount / maxAmount) * 7}em`
+})
+
