112cdef11e33b85eb989d3851316fd9de9bcc2dc
msiesse <msiesse@corefortech.com>
2022-09-16 11:21:59 +0200

    pretty good responsive even for very small screen

--- scripts/main.js
+++ scripts/main.js
@@ -29,13 +29,41 @@ const data = [
     }
 ]
 
+const changeHeightOfStick = (stickIcon, amount, maxAmount) => {
+    if (window.screen.availWidth <= 425)
+        stickIcon.style.height = `${(amount / maxAmount) * 8}em`
+    else
+        stickIcon.style.height = `${(amount / maxAmount) * 7}em`
+}
+
 const weekdays = ['mon', 'tue', 'wed', 'thu', 'fri', 'sat', 'sun']
 const maxAmount = Math.max(...data.map((elem) => elem.amount))
 weekdays.forEach((day, index) => {
-    const elem = document.getElementById(`stick-icon-${day}`)
-    elem.style.height = `${(data[index].amount / maxAmount) * 7}em`
-    elem.addEventListener("click", () => {
+    const stickIcon = document.getElementById(`stick-icon-${day}`)
+    changeHeightOfStick(stickIcon, data[index].amount, maxAmount)
 
+    const stickInfo = document.getElementById(`info-stick-${day}`)
+    stickInfo.innerHTML = `$${data[index].amount}`
+    stickIcon.addEventListener('focus', () => {
+        stickInfo.style.backgroundColor = 'hsl(25, 47%, 15%)'
+        stickInfo.style.zIndex = '1'
+    })
+    stickIcon.addEventListener('blur', () => {
+        stickInfo.style.backgroundColor = 'white'
+        stickInfo.style.zIndex = '0'
+    })
+    stickIcon.addEventListener('mouseover', () => {
+        stickInfo.style.backgroundColor = 'hsl(25, 47%, 15%)'
+        stickInfo.style.zIndex = '1'
+    })
+    stickIcon.addEventListener('mouseleave', () => {
+        if (document.activeElement !== stickIcon) {
+            stickInfo.style.backgroundColor = 'white'
+            stickInfo.style.zIndex = '0'
+        }
+    })
+    addEventListener('resize', () => {
+        changeHeightOfStick(stickIcon, data[index].amount, maxAmount)
     })
 })
 
